a09a278c800a1a17e826fa6973a6022df701ebd9
Ashok Bhattarai <eashokbhattarai@gmail.com>
2022-03-28 20:59:14 +0545

    initial commit

---
+++ AdvancedProgramming/Data/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using AdvancedProgramming.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProgramming.Data
+{
+    public class EmployeeContext: DbContext
+    {
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+
+    }
+}
